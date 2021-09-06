7d8f8331b3d3e6893ddf8c6358f436c78c9acd1c
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-09-06 02:17:24 +0100

    Moving to OOP approach

--- projecto/livro.rb
+++ projecto/livro.rb
@@ -1,2 +1,16 @@
+def preco_com_desconto(preco, desconto)
+    preco - (preco * desconto)
+end
+
+
 nome = "Uma paixão de verão"
-isbn = ""
+isbn = "342-65675756-1"
+numero_paginas = 120
+
+preco = 10_000
+desconto = 0.1
+
+preco_com_desconto = preco_com_desconto(preco, desconto)
+
+
+
