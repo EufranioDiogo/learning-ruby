8b76508b5b996633577b16620ed7340d59913946
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-09-20 01:03:19 +0100

    chore: adding arrays content

---
+++ Estruturas de Dados/01 - Arrays.rb
@@ -0,0 +1,12 @@
+numeros = [1, 2, 3]
+
+puts "numeros type: #{numeros}"
+puts "Primária Posição: #{numeros.first}"
+puts "Primária Posição: #{numeros.last}"
+
+# array << value_to_append
+
+numeros << 4
+
+puts "numeros length: #{numeros.length}"
+puts "numeros type: #{numeros}"
